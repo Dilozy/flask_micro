a499f38008c92201e97ee1a7d1aad17b7c14afee
dilozy <denfil228@yandex.ru>
2025-08-20 18:10:14 +0300

    Finished service_b api

---
+++ service_b/models.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import func
+
+from .extensions import db
+
+
+class ReceivedItem(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    recieved_at: Mapped[datetime] = mapped_column(default=func.now())
